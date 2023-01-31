f44b18ebf33b838fcc9ae28075a9fd0d277e2284
Alexandr-Romanenko <124072064+Alexandr-Romanenko@users.noreply.github.com>
2023-01-31 22:20:12 +0200

    Update str (2).py

--- str (2).py
+++ str (2).py
@@ -2,14 +2,20 @@
 # строку, т.к. после него нет служебного символи
 url = 'https:\www.youtube.com'
 print (url)
+print('\n')
 
 # НО если записать и после com поставить обратный слеш, произойдет экранирование
 # на другую строку
 url1 = 'https:\www.youtube.com\new'
 print (url1)
 # для таких случаев нужно писать два слеша, чтобы экранировал только 1-й
+print('\n')
+
+# you shold disolay yours code
+
 # Пример:
 url2 = 'https:\www.youtube.com\\new'
+url3 = 'https://github.com/Alexandr-Romanenko/Rep_for_study/edit/master/str%20(2).py'
 print (url2)
 # Также мы можем использовать специальный ЛИТЕРАЛ r, который означает
 # только для чтения, с ним запись будет работать без 2-х слешэй
@@ -24,7 +30,7 @@ print (url3)
 # через два слеша
 ##os.walk('D:\\')
 
-
+print("Hello my friend!")
 
 
 
