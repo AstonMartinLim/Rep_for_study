5caf85ac4ca12e1760fa8aee79780bdaab2ec4c2
Alexandr-Romanenko <124072064+Alexandr-Romanenko@users.noreply.github.com>
2023-02-04 09:50:57 +0200

    Update str (M) index.py

--- str (M) index.py
+++ str (M) index.py
@@ -5,13 +5,19 @@ r1 = str1.index ('welcome')
 print(r1)
 # будет искать 'е' между 5 и 10 индексами
 
-try to this way
+
+# try to this way it works but code more biggest
+c = -1
 for i in str1:
-  if i == 'e':
-    print (i)
-    
+    c += 1
+    if i == 'e':
+        print (c)
+        break
+
+        
 r2 = str1.index('e', 5, 10)
 r3 = str1.index('my w', 0, -1)
+
 
 # If the value is not found, the find() method returns -1,
 # but the index() method will raise an exception:
